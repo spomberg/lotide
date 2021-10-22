b2e2d70a5f0abcfb4bcb8009b072104a4c4c4962
Marcos Spomberg <marcosspomberg@gmail.com>
2021-10-22 02:55:21 +0000

    Lotide assertArraysEqual

---
+++ assertArraysEqual.js
@@ -0,0 +1,16 @@
+const assertArraysEqual = function(firstArray, secondArray) {
+  let equal = true;
+  if (firstArray.length !== secondArray.length){
+    equal = false;
+  } else for (let x = 0; x < firstArray.length && equal; x++) {
+    firstArray[x] === secondArray[x] ? equal = true : equal = false;
+  }
+  equal ? console.log(`✅✅✅ Assertion Passed:  ${firstArray} === ${secondArray}`) : 
+  console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+}
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
