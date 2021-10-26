45817d9e00ea5fe1f80674afd6f8912d03829746
Marcos Spomberg <marcosspomberg@gmail.com>
2021-10-26 23:04:51 +0000

    First commit

---
+++ eqObjects.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length != keys2.length) {
+    return false;
+  }
+
+  for (const value in object1) {
+    if (object1[value] !== object2[value]) {
+      return false;
+    } else return true;
+  }
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertEqual(eqObjects(ab, ba), true);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertEqual(eqObjects(ab, abc), false);
