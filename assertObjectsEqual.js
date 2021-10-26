20906dc67159b695900a22a50195dbe4ee7e7dfe
Marcos Spomberg <marcosspomberg@gmail.com>
2021-10-26 23:39:19 +0000

    First commit

---
+++ assertObjectsEqual.js
@@ -0,0 +1,43 @@
+const eqArrays = function(firstArray, secondArray) {
+  let equal = true;
+  if (firstArray.length !== secondArray.length) {
+    return false;
+  } else for (let x = 0; x < firstArray.length && equal; x++) {
+    firstArray[x] === secondArray[x] ? equal = true : equal = false;
+  }
+  return equal;
+};
+
+const eqObjects = function(object1, object2) {
+  const keys1 = Object.keys(object1);
+  const keys2 = Object.keys(object2);
+
+  if (keys1.length !== keys2.length) {
+    return false;
+  }
+
+  for (const value of keys1) {
+    if (Array.isArray(object1[value])) {
+      if (!eqArrays(object1[value], object2[value])) {
+        return false;
+      }
+    } else if (object1[value] !== object2[value]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const assertObjectEqual = function(actual, expected) {
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${inspect(actual)} !== ${inspect(expected)}`);
+};
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectEqual(ab, ba);
+assertObjectEqual(ab, abc);
+
