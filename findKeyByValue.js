bb0aa9dc86022f7e0807713b77f753a52f7a31e1
Marcos Spomberg <marcosspomberg@gmail.com>
2021-10-26 22:14:13 +0000

    First commit

---
+++ findKeyByValue.js
@@ -0,0 +1,26 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed:  ${actual} === ${expected}`);
+  } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(object, valueToFind) {
+  let result = undefined;
+
+  for (const value in object) {
+    if (value === valueToFind) {
+      result = value;
+    }
+  }
+
+  return result;
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
