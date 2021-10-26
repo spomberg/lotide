e379721d082ad279781ca4c55cc78fe736b41761
Marcos Spomberg <marcosspomberg@gmail.com>
2021-10-26 00:43:29 +0000

    First revision

--- letterPositions.js
+++ letterPositions.js
@@ -1,17 +1,17 @@
 const assertArraysEqual = function(firstArray, secondArray) {
   let equal = true;
-  if (firstArray.length !== secondArray.length){
+  if (firstArray.length !== secondArray.length) {
     equal = false;
   } else for (let x = 0; x < firstArray.length && equal; x++) {
     firstArray[x] === secondArray[x] ? equal = true : equal = false;
   }
-  equal ? console.log(`✅✅✅ Assertion Passed:  ${firstArray} === ${secondArray}`) : 
-  console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
+  equal ? console.log(`✅✅✅ Assertion Passed:  ${firstArray} === ${secondArray}`) :
+    console.log(`🛑🛑🛑 Assertion Failed: ${firstArray} !== ${secondArray}`);
 };
 
 const eqArrays = function(firstArray, secondArray) {
   let equal = true;
-  if (firstArray.length !== secondArray.length){
+  if (firstArray.length !== secondArray.length) {
     return false;
   } else for (let x = 0; x < firstArray.length && equal; x++) {
     firstArray[x] === secondArray[x] ? equal = true : equal = false;
@@ -26,19 +26,11 @@ const letterPositions = function(sentence) {
     if (sentence[x] === ' ') {
       continue;
     }
-    !results[sentence[x]] ? results[sentence[x]] = x : results.sentence[x].push(x);
+    !results[sentence[x]] ? results[sentence[x]] = [x] : results[sentence[x]].push(x);
   }
 
   return results;
 };
 
-const helloString = {
-  h: [0],
-  e: [1],
-  l: [2, 3],
-  o: [4]
-};
-
-console.log(letterPositions('hello'));
-assertArraysEqual(letterPositions('hello')['h'], helloString['h']);
+assertArraysEqual(letterPositions('hello').e, [1]);
 
