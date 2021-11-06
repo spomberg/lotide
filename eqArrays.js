7bb26b9b334b307372b6e29df5d5e2ebcee1151c
Marcos Spomberg <marcosspomberg@gmail.com>
2021-11-06 02:47:53 +0000

    Refactored code for recursion

--- eqArrays.js
+++ eqArrays.js
@@ -6,18 +6,23 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function(firstArray, secondArray) {
   let equal = true;
-  if (firstArray.length !== secondArray.length){
-    return false;
-  } else for (let x = 0; x < firstArray.length && equal; x++) {
+  if (firstArray.length !== secondArray.length) return false;
+    
+  for (let x = 0; x < firstArray.length && equal; x++) {
+    if (Array.isArray(firstArray[x])) {
+      if (eqArrays(firstArray[x], secondArray[x])) {
+        continue;
+      } else return false;
+    }
     firstArray[x] === secondArray[x] ? equal = true : equal = false;
   }
   return equal;
 }
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-
-console.log(eqArrays([1, 2, 3], [1, 2, 3])) // => true
-console.log(eqArrays([1, 2, 3], [3, 2, 1])) // => false
-
-console.log(eqArrays(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(eqArrays(["1", "2", "3"], ["1", "2", 3])) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true) // => true
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false) // => false
+assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false) // => false
+assertEqual(eqArrays([[2, [4, [5, 6]]], [4]], [[2, [4, [5, 6]]], [4]]), true) // => true
+assertEqual(eqArrays([[2, [4, [5, 6]]], [4]], [[2, [4, [5, 7]]], [4]]), false) // => false
+assertEqual(eqArrays([[2, [4, [5, [6, 7 , 8, 9]]]], [4]], [[2, [4, [5, [6, 7 , 8, 7]]]], [4]]), false) // => false
+assertEqual(eqArrays([[2, [4, [5, [6, 7 , [8, "a", "b", ["c"]], 9]]]], [4]], [[2, [4, [5, [6, 7 , [8, "a", "b", ["c"]], 9]]]], [4]]), true) // => true
