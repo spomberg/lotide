e60d9ca4efccd5a850e3cf2e591cb3047b27e2b2
Marcos Spomberg <marcosspomberg@gmail.com>
2021-11-06 02:19:26 +0000

    Refactored code for recursion

--- eqObjects.js
+++ eqObjects.js
@@ -14,36 +14,37 @@ const eqArrays = function(firstArray, secondArray) {
   return equal;
 };
 
-const eqObjects = function(object1, object2) {
+const eqObjects = (object1, object2) => {
   const keys1 = Object.keys(object1);
   const keys2 = Object.keys(object2);
 
-  if (keys1.length !== keys2.length) {
-    return false;
-  }
+  if(keys1.length !== keys2.length) return false;
 
-  for (const value of keys1) {
-    if (Array.isArray(object1[value])) {
-      if (!eqArrays(object1[value], object2[value])) {
-        return false;
+  for (const key of keys1) {
+    if (Array.isArray(object1[key])) {
+      if (!eqArrays(object1[key], object2[key])) return false;
+    } 
+    if(!Array.isArray(object1[key]) && !Array.isArray(object2[key])) {
+      if(typeof object1[key] === "object" && typeof object2[key] === "object") {
+        if (eqObjects(object1[key], object2[key])) {
+          delete object1[key];
+          delete object2[key];
+          return eqObjects(object1, object2);
+        } else return false;
       }
-    } else if (object1[value] !== object2[value]) {
+    }
+    if (object1[key] !== object2[key]) {
       return false;
     }
   }
   return true;
 };
 
-const ab = { a: "1", b: "2" };
-const ba = { b: "2", a: "1" };
-assertEqual(eqObjects(ab, ba), true);
+assertEqual(eqObjects({ a: { z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), true) // => true
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: { z: 1 }, b: 2 }), false)// => false
+assertEqual(eqObjects({ a: { y: 0, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
+assertEqual(eqObjects({ a: { y: {x: 1, d: 2}, z: 1 }, b: 2 }, { a: 1, b: 2 }), false) // => false
+assertEqual(eqObjects({ a: { y: {x: 1, d: 2}, z: 1 }, b: 2 }, { a: { y: {x: 1, d: 2}, z: 1 }, b: 2 }), true) // => true
+assertEqual(eqObjects({ a: { y: {x: 1, d: 2}, z: 1 }, b: 2 }, { a: { y: {a: 1, b: 2}, z: 1 }, b: 3 }), false) // => false
 
-const abc = { a: "1", b: "2", c: "3" };
-assertEqual(eqObjects(ab, abc), false);
-
-const cd = { c: "1", d: ["2", 3] };
-const dc = { d: ["2", 3], c: "1" };
-assertEqual(eqObjects(cd, dc), true);
-
-const cd2 = { c: "1", d: ["2", 3, 4] };
-assertEqual(eqObjects(cd, cd2), false);
+module.exports = eqObjects;
