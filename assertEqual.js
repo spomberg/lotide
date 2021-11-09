d978a906bfbcf4c21590c2158551839ae716f611
Marcos Spomberg <marcosspomberg@gmail.com>
2021-11-09 01:04:07 +0000

    Refactored code to separate tests from main files

--- assertEqual.js
+++ assertEqual.js
@@ -4,9 +4,4 @@ const assertEqual = function(actual, expected) {
   } else console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
 };
 
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("XXXX", "XXXX");
-assertEqual("Xxxx", "XXxx");
-assertEqual(1111, 1111);
-assertEqual(109977, 1456);
+module.exports = assertEqual;
---
+++ test/assertEqual.test.js
@@ -0,0 +1,8 @@
+const assertEqual = require('../assertEqual');
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("XXXX", "XXXX");
+assertEqual("Xxxx", "XXxx");
+assertEqual(1111, 1111);
+assertEqual(109977, 1456);
